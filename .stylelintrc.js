f469d9927a1664780ccf16858f163592abb89f50
Mitchel Cabuloy <mixxorz@gmail.com>
2020-10-22 13:09:38 +0800

    Set up tabler

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
     // See https://github.com/torchbox/stylelint-config-torchbox for rules.
     extends: 'stylelint-config-torchbox',
+    ignoreFiles: ['flare_portal/static_src/sass/vendor/*'],
 };
