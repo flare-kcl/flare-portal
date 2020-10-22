b3dfeafac03b0f599f9041ffe76faf40d62e1708
Mitchel Cabuloy <mixxorz@gmail.com>
2020-10-22 19:36:30 +0800

    Fix migrations

--- flare_portal/users/migrations/0001_initial.py
+++ flare_portal/users/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 
 import django.contrib.auth.models
 import django.contrib.auth.validators
-from django.db import migrations, models
 import django.utils.timezone
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -58,7 +58,7 @@ class Migration(migrations.Migration):
                 (
                     "first_name",
                     models.CharField(
-                        blank=True, max_length=30, verbose_name="first name"
+                        blank=True, max_length=150, verbose_name="first name"
                     ),
                 ),
                 (
