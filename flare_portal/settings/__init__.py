ed8f4cf5dcd1837c81e2c314f77aca60609308b9
Mitchel Cabuloy <mixxorz@gmail.com>
2020-10-22 16:24:11 +0800

    Add Github Workflows

--- flare_portal/settings/__init__.py
+++ flare_portal/settings/__init__.py
@@ -5,9 +5,9 @@
 #       'debug_toolbar',
 #    ]
 #
-# You will have `debug_toolbar` enabled even if you run
-# your application in production settings because `INSTALLED_APPS` will be changed during import
+# You will have `debug_toolbar` enabled even if you run your application in
+# production settings because `INSTALLED_APPS` will be changed during import
 #
-# It's better to explicitly specify settings file
-# by setting the `DJANGO_SETTINGS_MODULE` environment variable
-# or passing the --settings argument into your manage commands, when possible.
+# It's better to explicitly specify settings file by setting the
+# `DJANGO_SETTINGS_MODULE` environment variable or passing the --settings
+# argument into your manage commands, when possible.
