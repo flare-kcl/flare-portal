c6da8bcb1a1b91322924fa4502b76a0b54f9af3d
Mitchel Cabuloy <mixxorz@gmail.com>
2020-10-23 15:21:26 +0800

    Fix error pages

--- flare_portal/urls.py
+++ flare_portal/urls.py
@@ -45,14 +45,8 @@ if settings.DEBUG:
 
     urlpatterns += [
         # Add views for testing 404 and 500 templates
-        path(
-            "test404/",
-            TemplateView.as_view(template_name="patterns/pages/errors/404.html"),
-        ),
-        path(
-            "test500/",
-            TemplateView.as_view(template_name="patterns/pages/errors/500.html"),
-        ),
+        path("test404/", TemplateView.as_view(template_name="404.html"),),
+        path("test500/", TemplateView.as_view(template_name="500.html"),),
     ]
 
     # Try to install the django debug toolbar, if exists
--- flare_portal/utils/views.py
+++ flare_portal/utils/views.py
@@ -1,9 +1,9 @@
 from django.views import defaults
 
 
-def page_not_found(request, exception, template_name="patterns/pages/errors/404.html"):
+def page_not_found(request, exception, template_name="404.html"):
     return defaults.page_not_found(request, exception, template_name)
 
 
-def server_error(request, template_name="patterns/pages/errors/500.html"):
+def server_error(request, template_name="500.html"):
     return defaults.server_error(request, template_name)
