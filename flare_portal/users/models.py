7d9627a91a6905914030b575832e4d06bb52b687
Mitchel Cabuloy <mixxorz@gmail.com>
2020-10-28 17:28:58 +0800

    Update main header

--- flare_portal/users/models.py
+++ flare_portal/users/models.py
@@ -2,4 +2,33 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        parts = []
+
+        if self.first_name:
+            parts.append(self.first_name)
+
+        if self.last_name:
+            parts.append(self.last_name)
+
+        name = " ".join(parts)
+
+        if name:
+            return name
+
+        return self.username
+
+    @property
+    def initials(self):
+        letters = ""
+
+        if self.first_name:
+            letters += self.first_name[0]
+
+        if self.last_name:
+            letters += self.last_name[0]
+
+        if letters:
+            return letters.upper()
+
+        return self.username[0].upper()
