d5be2cc03fb234c445949439f87075b9746697df
Nathan <NathHorrigan@gmail.com>
2020-12-22 10:06:30 +0000

    Fix Migrations

--- flare_portal/experiments/migrations/0029_webmodule.py
+++ flare_portal/experiments/migrations/0029_webmodule.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("experiments", "0027_module_ordering"),
+        ("experiments", "0028_basic_info_unique"),
     ]
 
     operations = [
