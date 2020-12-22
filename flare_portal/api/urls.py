dc332811b41914046818e08070be2990575ae101
Mitchel Cabuloy <mixxorz@gmail.com>
2020-12-22 18:27:28 +0800

    #52 - Global settings (#79)
    * Add SiteConfiguration
    
    * Add views and urls
    
    * Fix "active" nav class implementation
    
    * Add site config to experiment config endpoint
    
    * Fix mypy issues
    
    * Add T&Cs API endpoint

--- flare_portal/api/urls.py
+++ flare_portal/api/urls.py
@@ -6,4 +6,9 @@ from .registry import data_api_registry
 app_name = "api"
 urlpatterns = [
     path("configuration/", views.configuration_api_view, name="configuration"),
+    path(
+        "terms-and-conditions/",
+        views.terms_and_conditions_api_view,
+        name="terms_and_conditions",
+    ),
 ] + data_api_registry.urls
---
+++ flare_portal/site_config/tests/test_models.py
@@ -0,0 +1,69 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from flare_portal.users.factories import UserFactory
+
+from ..models import SiteConfiguration
+
+
+class SiteSettingsTest(TestCase):
+    def test_settings(self) -> None:
+        # Load global settings
+        config = SiteConfiguration.get_solo()
+
+        self.assertEqual(config.terms_and_conditions, "")
+
+        config.terms_and_conditions = "T&Cs"
+
+        config.save()
+
+        config = SiteConfiguration.get_solo()
+
+        self.assertEqual(config.terms_and_conditions, "T&Cs")
+
+
+class UpdateSiteSettings(TestCase):
+    def test_permissions(self) -> None:
+        # Only admins can edit site settings
+        user = UserFactory()
+        user.grant_role("RESEARCHER")
+        user.save()
+
+        self.client.force_login(user)
+
+        url = reverse("site_config:update")
+
+        resp = self.client.get(url)
+        self.assertEqual(302, resp.status_code)
+
+        user.grant_role("ADMIN")
+        user.save()
+
+        resp = self.client.get(url)
+        self.assertEqual(200, resp.status_code)
+
+    def test_update(self) -> None:
+        user = UserFactory()
+        user.grant_role("ADMIN")
+        user.save()
+
+        self.client.force_login(user)
+
+        url = reverse("site_config:update")
+
+        resp = self.client.get(url)
+
+        self.assertEqual(200, resp.status_code)
+
+        form_data = {
+            "terms_and_conditions": "New terms",
+        }
+
+        resp = self.client.post(url, form_data, follow=True)
+
+        self.assertRedirects(resp, url)
+
+        self.assertEqual(
+            str(list(resp.context["messages"])[0]),
+            "Updated site configuration.",
+        )
