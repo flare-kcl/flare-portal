1eab66fe8f058505be6224065aabe18feb80ce04
Nathan <NathHorrigan@gmail.com>
2020-12-22 12:17:23 +0000

    Reorder T&C’s migration to fix misorder error

--- flare_portal/experiments/migrations/0030_participant_agreed_to_terms_and_conditions.py
+++ flare_portal/experiments/migrations/0030_participant_agreed_to_terms_and_conditions.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("experiments", "0027_module_ordering"),
+        ("experiments", "0029_webmodule"),
     ]
 
     operations = [
