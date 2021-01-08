281ca4b6c5b8e613c7d92937c102f6e2cc607a45
Nathan Horrigan <NathHorrigan@gmail.com>
2021-01-08 14:57:05 +0000

    Reorder migrations

--- flare_portal/experiments/migrations/0038_textmodule.py
+++ flare_portal/experiments/migrations/0038_textmodule.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('experiments', '0036_optional_rating'),
+        ('experiments', '0037_affectiveratingmodule'),
     ]
 
     operations = [
