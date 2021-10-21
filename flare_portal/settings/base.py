fedc0ede405a022cc58a84772c43bab6a102811f
Jake Howard <RealOrangeOne@users.noreply.github.com>
2021-10-21 15:39:54 +0100

    Disable Sentry's APM (#407)
    This massively incrases our quota usage, as each request becomes its own event, including those from Prometheus.

--- flare_portal/settings/base.py
+++ flare_portal/settings/base.py
@@ -403,10 +403,6 @@ if "SENTRY_DSN" in env and not (
     SENTRY_CONFIG = {
         "dsn": env["SENTRY_DSN"],
         "integrations": [DjangoIntegration()],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production,
-        "traces_sample_rate": 1.0,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         "send_default_pii": True,
